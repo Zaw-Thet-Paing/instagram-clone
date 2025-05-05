fa6c30f431f4890e6a1e66aee3de7b5975e93279
Zaw Thet Paing <zawthetpaing333@gmail.com>
2025-05-05 23:32:17 +0630

    instagram-clone: model mongo fixed

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Post extends Model
 {
